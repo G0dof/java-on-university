cdf9d2ad6e49575d606ff16748a58f535a0cef4d
g0dof <pedrorocha9@yahoo.com>
2022-11-23 15:56:23 -0300

    .

--- Aula08/src/Aula08/LerArquivo.java
+++ Aula08/src/Aula08/LerArquivo.java
@@ -5,36 +5,39 @@ import javax.swing.*;
 
 public class LerArquivo {
 
-  public static void main( String [] args ) {
-       JFileChooser qarquivo = new JFileChooser();
-       qarquivo.setDialogTitle("Abrir o arquivo");
+    public static void main(String[] args) {
+        JFileChooser qarquivo = new JFileChooser(); // Cria o objeto qarquivo da classe JFileChooser
+        qarquivo.setDialogTitle("Abrir o arquivo");
 
-       int opcao = qarquivo.showOpenDialog(new JFrame());
+        int opcao = qarquivo.showOpenDialog(new JFrame()); // Cria uma janela para selecionar o arquivo para ser lido
 
-       if (opcao == JFileChooser.APPROVE_OPTION) {
-          try
-          {
-          FileReader leitor  = new FileReader( qarquivo.getSelectedFile() );
-          BufferedReader leitorBuffer = new BufferedReader( leitor );
+        if (opcao == JFileChooser.APPROVE_OPTION) {
+            try {
+                FileReader leitor = new FileReader(qarquivo.getSelectedFile());
+                // Cria o objeto leitor da classe FileReader, e possui o parâmetro para ler o arquivo
 
-          String linha = "";
-          String texto = "";
-          while( linha != null ) {
-                linha = leitorBuffer.readLine( );  // ler a próxima linha
-                if(linha!=null) texto += linha + "\n";
-          }
-          leitorBuffer.close( );
-          leitor.close();
-          JOptionPane.showMessageDialog(null,texto);
-          }
-          catch(FileNotFoundException arquivo_nao_encontrado) {
-            JOptionPane.showMessageDialog(null, "O arquivo " + qarquivo.getSelectedFile() + " não existe.", "Erro fatal",JOptionPane.ERROR_MESSAGE );
-          }
-          catch (IOException erro_de_io) { //caso haja problemas na leitura do arquivo...
-            JOptionPane.showMessageDialog(null, "Outro erro.", "Erro", JOptionPane.ERROR_MESSAGE );
-          }
+                BufferedReader leitorBuffer = new BufferedReader(leitor);
+                // Servem para ler arquivos em formato texto
+
+                String linha = "";
+                String texto = "";
+                while (linha != null) {
+                    linha = leitorBuffer.readLine();  // ler a próxima linha
+                    if (linha != null) {
+                        texto += linha + "\n";
+                    }
+                }
+                leitorBuffer.close();
+                leitor.close();
+
+                JOptionPane.showMessageDialog(null, texto);
+            } catch (FileNotFoundException arquivo_nao_encontrado) {
+                JOptionPane.showMessageDialog(null, "O arquivo " + qarquivo.getSelectedFile() + " não existe.", "Erro fatal", JOptionPane.ERROR_MESSAGE);
+            } catch (IOException erro_de_io) { // Caso haja problemas na leitura do arquivo...
+                JOptionPane.showMessageDialog(null, "Outro erro.", "Erro", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        System.exit(0);
     }
-    System.exit(0);
-  }
 
 }
--- Aula08/src/Aula08/SalvarArquivo.java
+++ Aula08/src/Aula08/SalvarArquivo.java
@@ -6,42 +6,43 @@ import java.io.*;
 public class SalvarArquivo {
 
     public static void main(String[] args) {
-        JFileChooser seletorDeArquivo = new JFileChooser();
+        JFileChooser seletorDeArquivo = new JFileChooser(); //
         seletorDeArquivo.setDialogTitle("Selecione o arquivo para salvar...");
-     
+
         int opcSelecionada = seletorDeArquivo.showSaveDialog(new JFrame());
-     
+
         if (opcSelecionada == JFileChooser.APPROVE_OPTION) {
-              try {
-              FileWriter gravador = new FileWriter( seletorDeArquivo.getSelectedFile(), false );
-              //falso: significa que vamos criar um arquivo, este será deletado caso exista
-              //true: será usado se desejamos escrever no final do arquivo
-     
-              BufferedWriter bufGravador = new BufferedWriter(gravador);
-     
-              //vamos gravar algumas linhas no arquivo...
-              bufGravador.write( "Estas são várias linhas de texto. Esta é a primeira." );
-              bufGravador.newLine( );
-              bufGravador.write( "Agora gravamos a segunda linha de texto." );
-              bufGravador.newLine( );
-              bufGravador.write( "Esta será a terceira linha gravada." );
-              bufGravador.newLine( );
-              bufGravador.newLine( );
-              bufGravador.write( "E, finalmente, gravamos uma quarta linha." );
-              bufGravador.newLine( );
-     
-              //fechamos o arquivo:
-              bufGravador.close();
-              gravador.close();
-     
-              JOptionPane.showMessageDialog(null, "Gravação Ok...");
-              }
-              catch( Exception erro ) {
-                 JOptionPane.showMessageDialog(null, "Erro na gravação..." + erro.getMessage());
-              }
+            try {
+                FileWriter gravador = new FileWriter(seletorDeArquivo.getSelectedFile(), false);
+                // Cria o objeto leitor da classe FileReader, e possui o parâmetro para gravar um arquivo
+
+                //falso: significa que vamos criar um arquivo, este será deletado caso exista
+                //true: será usado se desejamos escrever no final do arquivo
+                BufferedWriter bufGravador = new BufferedWriter(gravador);
+                // Servem para escrever em arquivos de texto
+
+                // Vamos gravar algumas linhas no arquivo...
+                bufGravador.write("Estas são várias linhas de texto. Esta é a primeira.");
+                bufGravador.newLine();
+                bufGravador.write("Agora gravamos a segunda linha de texto.");
+                bufGravador.newLine();
+                bufGravador.write("Esta será a terceira linha gravada.");
+                bufGravador.newLine();
+                bufGravador.newLine();
+                bufGravador.write("E, finalmente, gravamos uma quarta linha.");
+                bufGravador.newLine();
+
+                //fechamos o arquivo:
+                bufGravador.close();
+                gravador.close();
+
+                JOptionPane.showMessageDialog(null, "Gravação Ok...");
+            } catch (Exception erro) { //caso haja problemas na leitura do arquivo...
+                JOptionPane.showMessageDialog(null, "Erro na gravação..." + erro.getMessage());
             }
-     
-            System.exit(0);
+        }
+
+        System.exit(0);
     }
-    
+
 }
