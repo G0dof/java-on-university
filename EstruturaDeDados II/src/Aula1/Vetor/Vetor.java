d7bfa186e6009c4ab74ea2ef04e64d951f3c6b5f
Gabriel dos Santos Silva <gabrieldossantossilva69@gmail.com>
2023-08-22 21:17:26 -0300

    Aula 2 feita

--- EstruturaDeDados II/src/Aula1/Vetor/Vetor.java
+++ EstruturaDeDados II/src/Aula1/Vetor/Vetor.java
@@ -1,7 +1,5 @@
 package Aula1.Vetor;
 
-import java.util.Arrays;
-
 public class Vetor {
     private String[] elementos;
     private int tamanho;
@@ -12,9 +10,7 @@ public class Vetor {
     }
 
     public void adiciona(String elemento) throws Exception {
-        if (this.tamanho > this.elementos.length)
-            throw new Exception("O vetor já está cheio, não é possível  adicionar novos elementos");
-
+        this.aumentaCapacidade();
         this.elementos[this.tamanho] = elemento;
         this.tamanho++;
     }
@@ -23,7 +19,57 @@ public class Vetor {
         return this.tamanho;
     }
 
-    public String toString(){
-        return Arrays.toString(elementos);
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+
+        for (int i = 0; i < this.tamanho - 1; i++) {
+            s.append(this.elementos[i]);
+            s.append(", ");
+        }
+
+        if (this.tamanho > 0) {
+            s.append(this.elementos[this.tamanho - 1]);
+        }
+
+        s.append("]");
+
+        return s.toString();
+    }
+
+    public String buscaPaia(int posicao) throws Exception {
+        if (posicao >= 0 && posicao < tamanho) return elementos[posicao];
+        else throw new Exception("Posição Inválida!");
+    }
+
+    public int buscaLinear(String buscado) {
+        for (int i = 0; i < tamanho; i++) {
+            if (elementos[i].equalsIgnoreCase(buscado)) return i;
+        }
+        return -1;
+    }
+
+    public boolean adicionaInicio(int posicao, String elemento) throws Exception {
+        if (!(posicao >= 0 && posicao < tamanho)) throw new Exception("Posição Inválida!");
+        for (int i = this.tamanho - 1; i >= posicao; i--) this.elementos[i + 1] = this.elementos[i];
+        this.elementos[posicao] = elemento;
+        this.tamanho++;
+
+        return true;
+    }
+
+    public void aumentaCapacidade() {
+        if (this.tamanho != this.elementos.length) return;
+
+        String[] elementosNovos = new String[this.elementos.length * 2];
+        for (int i = 0; i < this.elementos.length; i++) elementosNovos[i] = this.elementos[i];
+        this.elementos = elementosNovos;
+    }
+
+    public void remove(int posicao) throws Exception {
+        if (!(posicao >= 0 && posicao < tamanho)) throw new Exception("Posição Inválida!");
+        for (int i = posicao; i < this.tamanho - 1; i++) this.elementos[i] = this.elementos[i + 1];
+        this.tamanho--;
     }
 }
