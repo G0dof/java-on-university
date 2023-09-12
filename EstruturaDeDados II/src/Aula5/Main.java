72d8bb7eeeff9d556d00e9a2ab1ffde75f3857a3
g0dof <pedrorocha9@yahoo.com>
2023-09-12 20:40:43 -0300

    Aula5 feita

---
+++ EstruturaDeDados II/src/Aula5/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Fila<Integer> fila = new Fila<Integer>();
+        fila.enfileira(1);
+        fila.enfileira(2);
+        fila.enfileira(3);
+
+        System.out.println("A fila está vazia? " + fila.estaVazia());
+        System.out.println("Qual o tamanho da fila: " + fila.tamanho);
+        System.out.println(fila.toString());
+
+        System.out.println(fila.espiar());
+
+        if(fila.espiar() == null) System.out.println("Ninguem na fila");
+        else System.out.println("O elemento da primeira posição é: "+fila.espiar());
+    }
+}
